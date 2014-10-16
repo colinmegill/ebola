fc7c3b035954bc05b9ec2fb4060478f64491b789
Trevor Bedford <trvrb@trevorbedford.com>
2014-10-15 17:37:58 -0700

    Add button to separate new reports from cumulative reports.

--- js/site.js
+++ js/site.js
@@ -40,34 +40,39 @@ var svg = d3.select(".Chart").append("svg")
 
 svg.append("text")
   .attr("x", 50)
-  .attr("y", "75%")
+  .attr("y", "15%")
   .text("Deaths")
   .attr("class", "textDeath")
 
 svg.append("text")
   .attr("x", 50)
-  .attr("y", "70%")
+  .attr("y", "10%")
   .text("Cases")
   .attr("class", "textCases")
 
 function addAxis (data) {
 
-  x.domain(d3.extent(data, function(d) { return d.date; }));
-  y.domain(d3.extent(data, function(d) { return d.cases; }));
+	x.domain(d3.extent(data, function(d) { return d.date; }));
 
-  svg.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+	var max = d3.max(data, function(d) {
+    	return d.cases;
+	});
 
-  svg.append("g")
-      .attr("class", "yaxis")
-      .call(yAxis)
-      .append("text")
-      .attr("transform", "rotate(-90)")
-      .attr("y", 6)
-      .attr("dy", ".71em")
-      .style("text-anchor", "end");
+	y.domain([50, max]);	  
+
+	svg.append("g")
+		.attr("class", "x axis")
+		.attr("transform", "translate(0," + height + ")")
+		.call(xAxis);
+
+	svg.append("g")
+		.attr("class", "yaxis")
+		.call(yAxis)
+		.append("text")
+		.attr("transform", "rotate(-90)")
+		.attr("y", 6)
+		.attr("dy", ".71em")
+		.style("text-anchor", "end");
 }
 
 
@@ -110,13 +115,52 @@ function addDeathLine (data) {
 
 function visualize (data) {
 
-   data = _.each(data, function(d){
-    d.date = parseDate(d.date);
-  })
+	data = _.each(data, function(d){
+		d.date = parseDate(d.date);
+	});
+	
+	recentData = [];
+	data.forEach( function(d) {
+		if ( d.date.getTime() - parseDate("May 1").getTime() > 0 ) {
+			recentData.push(d);
+		}
+	});
+	data = recentData;
+	
+	cumulativeData = data;
+	
+	var prevCases = data[0].cases,
+		prevDeaths = data[0].deaths,
+		prevDate = data[0].date,
+		intervalData = [],
+		h = {};
+		h.date = prevDate;
+		h.cases = 0.1;
+		h.deaths = 0.1;
+		intervalData.push(h);
+		
+	data.slice(1).forEach( function(d) {
+		h = {};
+		var oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
+		var interval = (d.date.getTime() - prevDate.getTime()) / oneDay;			
+		h.date = d.date;
+		h.cases = (d.cases - prevCases) / interval;
+		h.deaths = (d.deaths - prevDeaths) / interval;
+		prevCases = d.cases;
+		prevDeaths = d.deaths;
+		prevDate = d.date;
+		if (h.cases == 0) {
+			h.cases = 0.1;
+		}
+		if (h.deaths == 0) {
+			h.deaths = 0.1;
+		}		
+		intervalData.push(h);
+	});	
 
-  addAxis(data);
-  addCasesLine(data);
-  addDeathLine(data);
+	addAxis(data);
+	addCasesLine(data);
+	addDeathLine(data);
 
 	d3.select("#log")
         .on("click", function(d,i) {
@@ -126,7 +170,19 @@ function visualize (data) {
 	d3.select("#linear")
         .on("click", function(d,i) {
             switchToLinear(data);
-		})  		
+		})  
+		
+	d3.select("#interval")
+        .on("click", function(d,i) {
+        	data = intervalData;
+            switchToInterval(data);
+		})  	
+		
+	d3.select("#cumulative")
+        .on("click", function(d,i) {
+        	data = cumulativeData;
+            switchToCumulative(data);
+		})  						
 
 };
 
@@ -137,8 +193,8 @@ function switchToLog(data) {
 	});
 
 	y = d3.scale.log()
-    .range([height, 0])
-    .domain([50, max]);	
+    	.range([height, 0])
+    	.domain(y.domain());	
     		
 	svg.selectAll(".casesline").datum(logFit(data))
 		.transition().duration(1000)
@@ -162,11 +218,11 @@ function switchToLog(data) {
 		
 	svg.selectAll(".textCases")
 		.transition().duration(1000)
-		.attr("y", "40%");		
+		.attr("y", "10%");		
 		
 	svg.selectAll(".textDeath")
 		.transition().duration(1000)
-		.attr("y", "45%");				
+		.attr("y", "15%");				
 		
 };
 
@@ -178,8 +234,8 @@ function switchToLinear(data) {
 
 	y = d3.scale.linear()
     	.range([height, 0])
-    	.domain([1, max]);
-    	
+    	.domain(y.domain());
+    	    	
 	svg.selectAll(".casesline").datum(logFit(data))
 		.transition().duration(1000)
 		.attr("d", casesLine);	
@@ -202,11 +258,87 @@ function switchToLinear(data) {
 		
 	svg.selectAll(".textCases")
 		.transition().duration(1000)
-		.attr("y", "70%");		
+		.attr("y", "10%");		
 		
 	svg.selectAll(".textDeath")
 		.transition().duration(1000)
-		.attr("y", "75%");				
+		.attr("y", "15%");				
+		
+};
+
+function switchToInterval(data) {
+
+	var max = d3.max(data, function(d) {
+    	return d.cases;
+	});
+
+	y.domain([1, max]);	// set domain
+    	
+	svg.selectAll(".casesline").datum(logFit(data))
+		.transition().duration(1000)
+		.attr("d", casesLine);	
+		
+	svg.selectAll(".deathline").datum(logFit(data))
+		.transition().duration(1000)
+		.attr("d", deathLine);	    	
+    	
+	svg.selectAll(".cases").data(data)
+    	.transition().duration(1000)
+    	.attr("cy", function(d) { return y(d.cases); });
+    	
+	svg.selectAll(".deaths").data(data)
+    	.transition().duration(1000)
+    	.attr("cy", function(d) { return y(d.deaths); });    	
+
+	svg.selectAll(".yaxis")
+		.transition().duration(1000)
+		.call(yAxis.scale(y));
+		
+	svg.selectAll(".textCases")
+		.transition().duration(1000)
+		.attr("y", "10%");		
+		
+	svg.selectAll(".textDeath")
+		.transition().duration(1000)
+		.attr("y", "15%");				
+		
+};
+
+function switchToCumulative(data) {
+
+	var max = d3.max(data, function(d) {
+    	return d.cases;
+	});
+
+	y.domain([50, max]);	// set domain
+    	
+	svg.selectAll(".casesline").datum(logFit(data))
+		.transition().duration(1000)
+		.attr("d", casesLine);	
+		
+	svg.selectAll(".deathline").datum(logFit(data))
+		.transition().duration(1000)
+		.attr("d", deathLine);	    	
+    	
+	svg.selectAll(".cases").data(data)
+    	.transition().duration(1000)
+    	.attr("cy", function(d) { return y(d.cases); });
+    	
+	svg.selectAll(".deaths").data(data)
+    	.transition().duration(1000)
+    	.attr("cy", function(d) { return y(d.deaths); });    	
+
+	svg.selectAll(".yaxis")
+		.transition().duration(1000)
+		.call(yAxis.scale(y));
+		
+	svg.selectAll(".textCases")
+		.transition().duration(1000)
+		.attr("y", "10%");		
+		
+	svg.selectAll(".textDeath")
+		.transition().duration(1000)
+		.attr("y", "15%");				
 		
 };
 
